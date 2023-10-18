b91ac0e4431130ac94c24f8b52bc3d8bc67378fc
Mohammad Malmir <m.malmir666@gmail.com>
2023-10-19 00:50:39 +0330

    complete htaccess

--- index.php
+++ index.php
@@ -1,14 +1,22 @@
 <?php
 function URL($key = null){
     
-     $arr=explode("/",trim($_GET['url'],"/"));
-     if(!is_numeric($key))
+     $arr=explode("/",trim($_GET['url']?? 'home',"/"));
+     if(!is_numeric($key) )
      return $arr;
 
     return $arr[$key] ?? '';
 }
-echo '<pre>';
-print_r(URL());
-echo '</pre>';
-echo 'page name is:' .URL(0);
+// echo '<pre>';
+// print_r(URL());
+// echo '</pre>';
+// echo 'page name is:' .URL(0);
+
+$file=URL(0).'.php';
+
+if(file_exists($file)){
+    require $file;
+}else{
+   require '404.php';
+}
 ?>
