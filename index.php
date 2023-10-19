8ac0ab97ef3deb09079524cc1c0b2725ed16b285
Mohammad Malmir <m.malmir666@gmail.com>
2023-10-19 22:35:14 +0330

    htaccess almost done

--- index.php
+++ index.php
@@ -1,19 +1,15 @@
 <?php
-function URL($key = null){
-    
+function URL($key = null){ 
      $arr=explode("/",trim($_GET['url']?? 'home',"/"));
-     if(!is_numeric($key) )
+     if(!is_numeric($key))
      return $arr;
-
     return $arr[$key] ?? '';
 }
 // echo '<pre>';
 // print_r(URL());
 // echo '</pre>';
 // echo 'page name is:' .URL(0);
-
 $file=URL(0).'.php';
-
 if(file_exists($file)){
     require $file;
 }else{
